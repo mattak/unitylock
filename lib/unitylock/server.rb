192011d8c2530803edd38bfcc1e2dddc3edcc62a
mattak <mattak.me@gmail.com>
2016-08-17 06:43:40 +0900

    Initialize gem files

---
+++ lib/unitylock/server.rb
@@ -0,0 +1,7 @@
+require "unitylock/server/version"
+
+module Unitylock
+  module Server
+    # Your code goes here...
+  end
+end
---
+++ spec/unitylock/server_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Unitylock::Server do
+  it 'has a version number' do
+    expect(Unitylock::Server::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
