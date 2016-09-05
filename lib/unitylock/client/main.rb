2d9fd0e0b23e4ac9208eea2ec5480e6ddc853a9f
mattak <mattak.me@gmail.com>
2016-09-06 01:08:26 +0900

    sync command added

--- lib/unitylock/client/main.rb
+++ lib/unitylock/client/main.rb
@@ -1,5 +1,6 @@
 require 'faraday'
 require 'json'
+require 'find'
 
 module Unitylock
   module Client
@@ -61,6 +62,27 @@ module Unitylock
           end.body
         end
         results
+      end
+
+      def sync(dir: '.', force: false, includes: /\.(unity|prefab)$/, excludes: /^$/)
+        files = Find.find(dir).select{|it| it =~ includes}.select{|it| it !~ excludes}
+        puts "touched:"
+        puts files
+
+        touches(files)
+        touched_files = JSON.parse(search.body).collect {|it| it['file']}
+
+        if force
+          remove_files = touched_files.select{|it| !files.include?(it) }
+
+          puts "removed:"
+          puts remove_files
+
+          deletes(remove_files)
+          return (touched_files - remove_files)
+        end
+
+        return touched_files
       end
     end
   end
